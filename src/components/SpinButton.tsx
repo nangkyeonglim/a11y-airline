d000ffcac01372843c11c5ccb3e9ac2b8763256b
nangkyeonglim <nangkyeonglim@gmail.com>
2023-07-28 14:40:27 +0900

    feat: 웹 접근성 구현

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,16 +1,24 @@
-import React, { useState, MouseEvent } from "react";
-import "./SpinButton.css";
+import React, { useState, MouseEvent } from 'react';
+import './SpinButton.css';
+
+const PASSENGER = {
+  MAX: 3,
+  MIN: 0,
+};
 
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
+  const [screenReaderMessage, setScreenReaderMessage] = useState('');
 
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
+    setScreenReaderMessage(`성인 승객 추가 ${count + 1}`);
   };
 
   const decrement = () => {
     setCount((prevCount) => prevCount - 1);
+    setScreenReaderMessage(`성인 승객 감소 ${count - 1}`);
   };
 
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
@@ -18,35 +26,60 @@ const SpinButton: React.FC = () => {
   };
 
   return (
-    <section className="spinButtonContainer">
+    <section className='spinButtonContainer'>
       <div>
         <h1>승객 선택</h1>
-        <div className="spinButtonLabel">
+        <div className='spinButtonLabel'>
           <label>성인</label>
           <div
-            className="helpIcon"
+            className='helpIcon'
             onMouseEnter={toggleTooltip}
             onMouseLeave={toggleTooltip}
           >
             ?
             {isTooltipVisible && (
-              <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
+              <span className='tooltip'>최대 인원수는 3명까지 가능합니다</span>
             )}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button
+          onClick={decrement}
+          className='spinButton'
+          aria-label='성인 탑승자 한명 줄이기'
+          disabled={count === PASSENGER.MIN}
+        >
           -
         </button>
         <input
-          type="text"
-          role="spinbutton"
+          type='text'
+          role='spinbutton'
           readOnly
-          className="spinButtonInput"
+          className='spinButtonInput'
           value={count}
+          aria-valuemin={PASSENGER.MIN}
+          aria-valuemax={PASSENGER.MAX}
+          aria-label={`성인 ${count} 텍스트 숫자만 수정`}
         />
-        <button onClick={increment} className="spinButton">
+        <button
+          onClick={increment}
+          className='spinButton'
+          aria-label='성인 탑승자 한명 늘리기'
+          disabled={count === PASSENGER.MAX}
+        >
           +
         </button>
+        <div
+          id='liveRegion'
+          aria-live='assertive'
+          style={{
+            position: 'absolute',
+            overflow: 'hidden',
+            width: '0px',
+            height: '0px',
+          }}
+        >
+          {screenReaderMessage}
+        </div>
       </div>
     </section>
   );
